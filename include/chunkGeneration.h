ef62fc3cb733cfb5632669151152f158214783f2
D3crvpt3d <st4r.sprayer@gmail.com>
2025-02-24 19:20:11 +0100

    save

--- include/chunkGeneration.h
+++ include/chunkGeneration.h
@@ -6,10 +6,11 @@
 #include <GLFW/glfw3.h>
 
 
-#define CHUNK_WDH 8
+#define CHUNK_WD 64	//chunk width/depth
+#define CHUNK_H 16	//chunk height
+#define BLOCKS_PER_CHUNK CHUNK_WD * CHUNK_WD * CHUNK_H
 #define RENDERDISTANCE 2
 #define CHUNK_THREADS 4
-#define BLOCKS_PER_CHUNK CHUNK_WDH * CHUNK_WDH * CHUNK_WDH
 
 #define RENDERSPAN (2*RENDERDISTANCE+1) //from x-renderdistance to x+renderdistance
 #define CHUNKS RENDERSPAN * RENDERSPAN * RENDERSPAN
@@ -23,12 +24,13 @@ typedef struct{
 	int32_t z;
 } vec3i_t;
 
+//to check if draw: block[z][y][x] && visible
 //raw chunk where each block is uint as blocktype (0 := air, so that "if(block[x][y][z])" works)
 typedef struct{
 	vec3i_t pos;
 	uint8_t visible[6];//visible faces
 	uint32_t numBlocks;
-	uint8_t block[CHUNK_WDH][CHUNK_WDH][CHUNK_WDH];
+	uint8_t block[BLOCKS_PER_CHUNK];//chunk_wdh^3
 } Chunk;
 
 //holds the actual raw chunk data
