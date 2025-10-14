81a1b383b93046b6ea4478c3ec0805059f9836cc
D3crypt3d <d3crvpt3d@d3crypt3d.com>
2025-10-15 00:14:59 +0200

    save

--- include/optimize_buffer.h
+++ include/optimize_buffer.h
@@ -10,6 +10,10 @@ typedef struct {
 	GLuint type;
 } BlockGPU_t;
 
+static_assert(sizeof(BlockGPU_t) == 16, "BlockGPU_t must be 16 bytes!");
+static_assert(offsetof(BlockGPU_t, xyz) == 0, "xyz offset must be 0");
+static_assert(offsetof(BlockGPU_t, type) == 12, "type offset must be 12");
+
 std::vector<BlockGPU_t> gen_optimized_buffer(Chunk_t &chunk);
 
 #endif
